b72cdbe16c25a50feade2f94acd2d1190bd7e3ec
acid7820@gmail.com <acid7820@gmail.com>
2019-09-30 16:32:14 +0800

    修改publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   lintOnSave: false,
   publicPath:
-    process.env.NODE_ENV === "production" ? "vuex_simple_employee" : "/"
+    process.env.NODE_ENV === "production" ? "/vuex_simple_employee/" : "/"
 };
